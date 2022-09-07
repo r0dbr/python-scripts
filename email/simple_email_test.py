498e638816ed530f3fe7f1e8de73d4795efaa293
Rodrigo Corrêa de Carvalho <rodrigo@rodbr.com>
2022-09-07 16:21:35 -0400

    Update simple_email_test.py

--- email/simple_email_test.py
+++ email/simple_email_test.py
@@ -25,41 +25,41 @@ SENHA = 'password'
 
 def imap_test():
     try :
-        print '* Testing your IMAP connection:'
+        print('* Testing your IMAP connection:')
         mail = imaplib.IMAP4_SSL(IMAP_SERVER)
         mail.login(LOGIN, SENHA)
         mail.list()
-        print '    Nice! Everytihing is OK with you server and credentials'
-        print mail.select("inbox") 
+        print('    Nice! Everytihing is OK with you server and credentials')
+        print(mail.select("inbox") )
     except :
-        print '    Fail to connect on your IMAP  server %s' % (IMAP_SERVER)
+        print('    Fail to connect on your IMAP  server %s' % (IMAP_SERVER))
 
 
 def pop3_test():    
-
+    
     try:
-        print '* Testing your POP3 connection:'
+        print('* Testing your POP3 connection:')
         M = poplib.POP3(POP_SERVER)
         M.user(LOGIN)
         M.pass_(SENHA)
-        print '    Nice! There is %d messages on your server %s ' % (len(M.list()[1]),POP_SERVER)
+        print('    Nice! There is %d messages on your server %s ' % (len(M.list()[1]),POP_SERVER))
     except :
-        print '    Oh Oh! Error trying to connect on your POP3 server %s' % (POP_SERVER)
+        print('    Oh Oh! Error trying to connect on your POP3 server %s' % (POP_SERVER))
 
 def pop3ssl_test():    
-
+    
     try:
-        print '* Testando conexao POP3 SSL:'
+        print('* Testando conexao POP3 SSL:')
         M = poplib.POP3_SSL(POP_SERVER)
-        print M.getwelcome()
+        print(M.getwelcome())
         M.user(LOGIN)
         M.pass_(SENHA)
-        print '    Nice! There is %d messages on your server %s ' % (len(M.list()[1]),POP_SERVER)
+        print('    Nice! There is %d messages on your server %s ' % (len(M.list()[1]),POP_SERVER))
     except :
-        print '    Oh Oh! Error trying to connect on your POP3 server %s' % (POP_SERVER)
+        print('    Oh Oh! Error trying to connect on your POP3 server %s' % (POP_SERVER))
 
 def smtp_test():     
-    print '* Testing SMTP connection to %s ' % (recipient)
+    print('* Testing SMTP connection to %s ' % (recipient))
     subject = 'SMTP Test'
     body = 'blah blah blah'
      
